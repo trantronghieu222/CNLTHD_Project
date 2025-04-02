e48fd9e869295ddbe6a0f0e6afc951b460370969
Tran Trong Hieu <tranhieu4345@gmail.com>
2025-04-02 13:34:23 +0700

    cập nhật error code của account-service

--- Code/Microservice/account-service/src/main/java/com/shop/accountservice/exception/ErrorCode.java
+++ Code/Microservice/account-service/src/main/java/com/shop/accountservice/exception/ErrorCode.java
@@ -3,18 +3,18 @@ package com.shop.accountservice.exception;
 import java.time.OffsetDateTime;
 
 public enum ErrorCode {
-    USER_NOT_FOUND(404, "USER_NOT_FOUND", "User not found."),
-    USERNAME_ALREADY_EXISTS(409, "USERNAME_ALREADY_EXISTS", "The username already exists in the system."),
+    USER_NOT_FOUND(404, "USER_NOT_FOUND", "Không tìm thấy người dùng."),
+    USERNAME_ALREADY_EXISTS(409, "USERNAME_ALREADY_EXISTS", "Tên người dùng đã tồn tại trong hệ thống."),
     ;
 
     private final int statusCode;
     private final String errorCode;
-    private final String message;// Thêm timestamp
+    private final String message;
 
     ErrorCode(int statusCode, String errorCode, String message) {
         this.statusCode = statusCode;
         this.errorCode = errorCode;
-        this.message = message;// Gán timestamp khi tạo lỗi
+        this.message = message;
     }
 
     public int getStatusCode() {
