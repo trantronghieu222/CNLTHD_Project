ef9e1d730bcd91cf7793e3a5ce300e62b4e413e6
anhpham17092003 <153734641+anhpham17092003@users.noreply.github.com>
2025-05-21 01:31:35 +0700

    update ProductClient

--- Code/Microservice/received-service/src/main/java/com/shop/receivedservice/Client/ProductClient.java
+++ Code/Microservice/received-service/src/main/java/com/shop/receivedservice/Client/ProductClient.java
@@ -1,5 +1,12 @@
 package com.shop.receivedservice.Client;
 
-public class ProductClient {
-    
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "product-service")
+public interface ProductClient {
+    @GetMapping("/product/{id}")
+    void getProductById (@PathVariable Integer id);
 }
