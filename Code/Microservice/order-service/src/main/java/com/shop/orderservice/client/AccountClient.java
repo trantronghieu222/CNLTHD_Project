fbb700dcf0f42b388829904382382f68e5fc2946
anhpham17092003 <153734641+anhpham17092003@users.noreply.github.com>
2025-05-21 01:08:08 +0700

    update AccountClient

--- Code/Microservice/order-service/src/main/java/com/shop/orderservice/client/AccountClient.java
+++ Code/Microservice/order-service/src/main/java/com/shop/orderservice/client/AccountClient.java
@@ -3,6 +3,7 @@ package com.shop.orderservice.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "account-service")
 public interface AccountClient {
