b00c22f55e9f5f8cf10889b15880961dec9896a9
Tran Trong Hieu <tranhieu4345@gmail.com>
2025-05-21 00:14:36 +0700

    update config yml

--- Code/Microservice/order-service/src/main/java/com/shop/orderservice/OrderServiceApplication.java
+++ Code/Microservice/order-service/src/main/java/com/shop/orderservice/OrderServiceApplication.java
@@ -1,12 +1,12 @@
 package com.shop.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableFeignClients
-//@EnableDiscoveryClient
+@EnableFeignClients
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
